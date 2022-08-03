ab48ff2562182768fe1e11b4dad37c9f85d02750
Rhomeinel <rhomeinel@gmail.com>
2022-08-03 16:19:05 -0700

    Update with do_destory command

--- console.py
+++ console.py
@@ -23,14 +23,14 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program at end of file"""
         print()
         return True
-    
+
     def emptyline(self):
         """Ingnore empty line"""
         pass
 
     def do_create(self, line):
         """
-        Creates a new instance of BaseModel, saves it 
+        Creates a new instance of BaseModel, saves it
         (to the JSON file) and prints the id.
         """
         if line == "" or line is None:
@@ -44,13 +44,13 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, line):
         """
-        Prints the string representation of an instance 
+        Prints the string representation of an instance
         based on the class name and id
         """
         inpu = line.slip()
         if line == "" or line is None:
             print("** class name missing **")
-        elif inpu[0]in self.class_list:
+        elif inpu[0] in self.class_list:
             if len(inpu) < 2:
                 print("** instance id missing **")
             else:
@@ -61,14 +61,28 @@ class HBNBCommand(cmd.Cmd):
                     print("** no instance found **")
         else:
             print("** class doesn't exist **")
-    
 
-
-
-
-
+    def do_destory(self, line):
+        """
+        Deletes an instance based on the class name and id
+        line.slip((save the change into the JSON file)
+        """
+        inpu = line.slip()
+        if line == "" or line is None:
+            print("** class name missing **")
+        elif inpu[0] in self.class_list:
+            if len(inpu) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(inpu[0], inpu[1])
+                if key in storage.all():
+                    storage.all().pop(key)
+                    storage.save()
+                else:
+                    print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-        
