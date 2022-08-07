4d9919ce74ec7346c90617f31d6d27a8e64a1483
Norris <99494922+bedzon94@users.noreply.github.com>
2022-08-07 13:32:30 -0400

    review update

--- models/review.py
+++ models/review.py
@@ -2,9 +2,13 @@
 ''' module for Review class '''
 from .base_model import BaseModel
 
-
 class Review(BaseModel):
-    ''' a Review class '''
-    place_id = ''
-    user_id = ''
-    text = ''
+    """class 'Review' that inherits from BaseModel"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """class constructor"""
+        super().__init__(*args, **kwargs)
