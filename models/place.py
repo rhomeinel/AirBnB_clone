008f92e003dea7f3446234c556a024a3b26ebc27
Norris <99494922+bedzon94@users.noreply.github.com>
2022-08-07 13:30:29 -0400

    place.py fix

--- models/place.py
+++ models/place.py
@@ -2,13 +2,13 @@
 ''' module for Place class '''
 from .base_model import BaseModel
 
-
 class Place(BaseModel):
-    ''' a Place class '''
-    city_id = ''
-    user_id = ''
-    name = ''
-    description = ''
+    """class 'Place' that inherits from BaseModel"""
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
     number_rooms = 0
     number_bathrooms = 0
     max_guest = 0
@@ -16,3 +16,7 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """class constructor"""
+        super().__init__(*args, **kwargs)
